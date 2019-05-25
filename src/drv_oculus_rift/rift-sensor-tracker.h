de46986ddeda1f5fe21dab890120e7a2fba51cb7
Jan Schmidt <jan@centricular.com>
2019-05-25 17:38:38 +1000

    Rift: Send radio address for camera sync
    Transfer the radio id read from the HMD to the sensor
    tracker code so the camera can synchronise -
    and we start getting blob reports

--- src/drv_oculus_rift/rift-sensor-tracker.h
+++ src/drv_oculus_rift/rift-sensor-tracker.h
@@ -1,5 +1,5 @@
 
 typedef struct rift_sensor_ctx_s rift_sensor_ctx;
 
-int rift_sensor_tracker_init (rift_sensor_ctx **ctx);
+int rift_sensor_tracker_init (rift_sensor_ctx **ctx, const uint8_t radio_id[5]);
 void rift_sensor_tracker_free (rift_sensor_ctx *ctx);
