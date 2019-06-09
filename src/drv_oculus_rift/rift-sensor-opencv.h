13ffb32027044770c73b8c5a7689162ca521058c
Jan Schmidt <jan@centricular.com>
2019-06-09 19:02:58 +1000

    rift: Add plausible opencv integration
    Integrate code from ouvrt for reading Rift Sensor calibration data
    that compiles but hasn't been tested. The flicker pattern
    identification is still missing for now.

---
+++ src/drv_oculus_rift/rift-sensor-opencv.h
@@ -0,0 +1,36 @@
+/*
+ * Pose estimation using OpenCV
+ * Copyright 2015 Philipp Zabel
+ * SPDX-License-Identifier:	LGPL-2.0+ or BSL-1.0
+ */
+#ifndef __OPENCV_H__
+#define __OPENCV_H__
+
+#include "rift.h"
+#include "rift-sensor-maths.h"
+
+#if HAVE_OPENCV
+void estimate_initial_pose(struct blob *blobs, int num_blobs,
+			   rift_led *leds, int num_leds,
+			   dmat3 *camera_matrix, double dist_coeffs[5],
+			   dquat *rot, dvec3 *trans, bool use_extrinsic_guess);
+#else
+static inline
+void estimate_initial_pose(struct blob *blobs, int num_blobs,
+			   rift_led *leds, int num_leds,
+			   dmat3 *camera_matrix, double dist_coeffs[5],
+			   dquat *rot, dvec3 *trans, bool use_extrinsic_guess)
+{
+	(void)blobs;
+	(void)num_blobs;
+	(void)leds;
+	(void)num_leds;
+	(void)camera_matrix;
+	(void)dist_coeffs;
+	(void)rot;
+	(void)trans;
+	(void)use_extrinsic_guess;
+}
+#endif /* HAVE_OPENCV */
+
+#endif /* __OPENCV_H__ */
--- src/drv_oculus_rift/rift-sensor-tracker.h
+++ src/drv_oculus_rift/rift-sensor-tracker.h
@@ -1,5 +1,7 @@
+#include "rift.h"
 
 typedef struct rift_sensor_ctx_s rift_sensor_ctx;
 
-int rift_sensor_tracker_init (rift_sensor_ctx **ctx, const uint8_t radio_id[5]);
+int rift_sensor_tracker_init (rift_sensor_ctx **ctx, const uint8_t radio_id[5],
+    rift_led *leds, uint8_t num_leds);
 void rift_sensor_tracker_free (rift_sensor_ctx *ctx);
