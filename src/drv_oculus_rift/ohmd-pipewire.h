45c91f66a0fc2c13f2ef4f26d2e0959a614fe746
Jan Schmidt <jan@centricular.com>
2019-09-17 17:42:04 +1000

    pipewire: Video stream publishing framework

--- src/drv_oculus_rift/ohmd-pipewire.h
+++ src/drv_oculus_rift/ohmd-pipewire.h
@@ -7,3 +7,16 @@
 #pragma once
 
 #include "../openhmdi.h"
+
+typedef struct ohmd_pw_video_stream_s ohmd_pw_video_stream;
+
+#ifdef HAVE_PIPEWIRE
+ohmd_pw_video_stream *ohmd_pw_video_stream_new (int w, int h, int fps_n, int fps_d);
+void ohmd_pw_video_stream_push (ohmd_pw_video_stream *v, uint8_t *pixels);
+void ohmd_pw_video_stream_free (ohmd_pw_video_stream *v);
+#else
+#define ohmd_pw_video_stream_new(w,h,fps_n,fps_d) NULL
+#define ohmd_pw_video_stream_push(v, pixels)
+#define ohmd_pw_video_stream_free(v);
+#endif
+
