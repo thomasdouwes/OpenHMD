e51d95d71d74ae6139da0704a4090d2c430ac839
Jan Schmidt <jan@centricular.com>
2019-12-22 02:07:47 +1100

    rift: Add draw_projected_leds_at() function

--- src/drv_oculus_rift/debug_draw.c
+++ src/drv_oculus_rift/debug_draw.c
@@ -2,6 +2,8 @@
 #include "rift-sensor-tracker.h"
 #include "rift-sensor-opencv.h"
 
+#include "debug_draw.h"
+
 #define WRITE_UINT24_BE(dest, colour) dest[0] = colour & 0xff; dest[1] = colour >> 8 & 0xff; dest[2] = colour >> 16 & 0xff
 
  #define MAX(a,b) \
@@ -159,7 +161,7 @@ draw_blob_debug_stuff(rift_sensor_ctx *sensor_ctx, struct rift_sensor_uvc_stream
       }
 
       /* Draw a purple box around unknown blobs. Green around recognised ones */
-      // draw_rgb_rect (out_frame->data[0], width, out_stride, height, start_x, start_y, b->width, b->height, b->led_id == -1 ? 0xFF00FF : 0x00FF00);
+      draw_rgb_rect (stream->debug_frame, width, out_stride, height, start_x, start_y, b->width, b->height, b->led_id == -1 ? 0xFF00FF : 0x00FF00);
     }
 #if DUMP_BLOBS
      g_print ("\n");
@@ -169,7 +171,12 @@ draw_blob_debug_stuff(rift_sensor_ctx *sensor_ctx, struct rift_sensor_uvc_stream
 
 void
 draw_projected_leds(rift_sensor_ctx *sensor_ctx, rift_leds *leds, struct rift_sensor_uvc_stream * stream) {
+  draw_projected_leds_at (sensor_ctx, leds, stream, &sensor_ctx->pose_orient, &sensor_ctx->pose_pos, 0xFF0000);
+}
 
+void
+draw_projected_leds_at(rift_sensor_ctx *sensor_ctx, rift_leds *leds, struct rift_sensor_uvc_stream * stream, quatf *rot, vec3f *trans, uint32_t color1)
+{
   // int x, y;
   // uint8_t *src = stream->frame; //in_frame->data[0];
   uint8_t *dest = stream->debug_frame; // out_frame->data[0];
@@ -188,7 +195,7 @@ draw_projected_leds(rift_sensor_ctx *sensor_ctx, rift_leds *leds, struct rift_se
   /* Project HMD LEDs into the image */
   rift_project_points(leds->points, leds->num_points,
       &sensor_ctx->camera_matrix, sensor_ctx->dist_coeffs,
-      &sensor_ctx->pose_orient, &sensor_ctx->pose_pos,
+      rot, trans,
       sensor_ctx->led_out_points);
 
   /* Check how many LEDs have matching blobs in this pose,
@@ -197,13 +204,18 @@ draw_projected_leds(rift_sensor_ctx *sensor_ctx, rift_leds *leds, struct rift_se
   int visible_leds = 0;
   for (i = 0; i < leds->num_points; i++) {
     vec3f *p = sensor_ctx->led_out_points + i;
-    vec3f facing;
     int x = round(p->x);
     int y = round(p->y);
 
-    oquatf_get_rotated(&sensor_ctx->pose_orient, &leds->points[i].dir, &facing);
+    vec3f normal, position;
+    double facing_dot;
 
-    if (facing.z < -0.5) {
+    oquatf_get_rotated(rot, &leds->points[i].pos, &position);
+    ovec3f_add (trans, &position, &position);
+
+    oquatf_get_rotated(rot, &leds->points[i].dir, &normal);
+    facing_dot = ovec3f_get_dot (&position, &normal);
+    if (facing_dot < -0.5) {
       /* Strongly Camera facing */
       struct blob *b = blobwatch_find_blob_at(sensor_ctx->bw, x, y);
       visible_leds++;
@@ -217,32 +229,37 @@ draw_projected_leds(rift_sensor_ctx *sensor_ctx, rift_leds *leds, struct rift_se
   if (visible_leds > 4 && matched_visible_blobs > 4) {
     if (sensor_ctx->bwobs->num_led_blobs < 2 * matched_visible_blobs) {
       good_pose_match = true;
-      // g_print ("  Found good pose match - %u LEDs matched %u visible ones\r",
-      //     matched_visible_blobs, visible_leds);
+      printf ("  Found good pose match - %u LEDs matched %u visible ones\r",
+         matched_visible_blobs, visible_leds);
     }
   }
 
   for (i = 0; i < leds->num_points; i++) {
     vec3f *p = sensor_ctx->led_out_points + i;
-    vec3f facing;
     int x = round(p->x);
     int y = round(p->y);
 
-    oquatf_get_rotated(&sensor_ctx->pose_orient, &leds->points[i].dir, &facing);
+    vec3f normal, position;
+    double facing_dot;
 
-    if (facing.z < 0) {
+    oquatf_get_rotated(rot, &leds->points[i].pos, &position);
+    ovec3f_add (trans, &position, &position);
+
+    oquatf_get_rotated(rot, &leds->points[i].dir, &normal);
+    facing_dot = ovec3f_get_dot (&position, &normal);
+    if (facing_dot < 0) {
       /* Camera facing */
       if (good_pose_match) {
         /* Back project LED ids into blobs if we find them and the dot product
           * shows them pointing strongly to the camera */
         struct blob *b = blobwatch_find_blob_at(sensor_ctx->bw, x, y);
-        if (b != NULL && facing.z < -0.5 && b->led_id != i) {
+        if (b != NULL && facing_dot < -0.5 && b->led_id != i) {
           /* Found a blob! */
-          // g_print ("Marking LED %d at %d,%d\n", i, x, y);
+          printf ("Marking LED %d at %d,%d\n", i, x, y);
           b->led_id = i;
         }
       }
-      draw_rgb_marker (dest, width, out_stride, height, x, y, 8, 8, 0xFF0000);
+      draw_rgb_marker (dest, width, out_stride, height, x, y, 8, 8, color1);
     } else {
       draw_rgb_marker (dest, width, out_stride, height, x, y, 8, 8, 0x202000);
     }
--- src/drv_oculus_rift/debug_draw.h
+++ src/drv_oculus_rift/debug_draw.h
@@ -1,10 +1,11 @@
 
-#include "rift-sensor-uvc.h"
-
 #ifndef DEBUG_DRAW_H
 #define DEBUG_DRAW_H
 
+#include "rift-sensor-uvc.h"
+
 void draw_blob_debug_stuff(rift_sensor_ctx *sensor_ctx, struct rift_sensor_uvc_stream * stream);
 void draw_projected_leds(rift_sensor_ctx *sensor_ctx, rift_leds *leds, struct rift_sensor_uvc_stream * stream);
+void draw_projected_leds_at(rift_sensor_ctx *sensor_ctx, rift_leds *leds, struct rift_sensor_uvc_stream * stream, quatf *rot, vec3f *trans, uint32_t color1);
 
 #endif
