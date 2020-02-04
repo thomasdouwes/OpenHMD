cdf05814456ed8cab113bf9460d3ad9d1c1cf8db
Philipp Zabel <philipp.zabel@gmail.com>
2020-02-04 22:26:41 +0100

    rift: do not crash if the manufacturer string is not set
    Do not crash if the Rift DK2 USB hub appears as 2833:0021 without a
    manufacturer string set.

--- src/drv_oculus_rift/rift.c
+++ src/drv_oculus_rift/rift.c
@@ -1170,7 +1170,8 @@ static void get_device_list(ohmd_driver* driver, ohmd_device_list* list)
 		while (cur_dev) {
 			// We need to check the manufacturer because other companies (eg: VR-Tek)
 			// are reusing the Oculus DK1 USB ID for their own HMDs
-			if((wcscmp(cur_dev->manufacturer_string, L"Oculus VR, Inc.") == 0) &&
+			if(cur_dev->manufacturer_string &&
+			   (wcscmp(cur_dev->manufacturer_string, L"Oculus VR, Inc.") == 0) &&
 			   (rd[i].iface == -1 || cur_dev->interface_number == rd[i].iface)) {
 				int id = 0;
 				ohmd_device_desc* desc = &list->devices[list->num_devices++];
