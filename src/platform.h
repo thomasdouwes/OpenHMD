722a3d6b0ea350b57fe5a8d8684b8004e86ebe2f
Jan Schmidt <jan@centricular.com>
2020-09-26 23:52:53 +1000

    Add conditional variable implementation
    Wrap pthread_cond_t on posix, and ConditionalVariable on Windows.

--- src/platform.h
+++ src/platform.h
@@ -18,12 +18,20 @@ void ohmd_toggle_ovr_service(int state);
 
 typedef struct ohmd_thread ohmd_thread;
 typedef struct ohmd_mutex ohmd_mutex;
+typedef struct ohmd_cond ohmd_cond;
 
 ohmd_mutex* ohmd_create_mutex(ohmd_context* ctx);
 void ohmd_destroy_mutex(ohmd_mutex* mutex);
 
 void ohmd_lock_mutex(ohmd_mutex* mutex);
 void ohmd_unlock_mutex(ohmd_mutex* mutex);
+
+ohmd_cond* ohmd_create_cond(ohmd_context* ctx);
+void ohmd_destroy_cond(ohmd_cond* cond);
+
+void ohmd_cond_wait(ohmd_cond* cond, ohmd_mutex* mutex);
+void ohmd_cond_signal(ohmd_cond* cond);
+void ohmd_cond_broadcast(ohmd_cond* cond);
 
 ohmd_thread* ohmd_create_thread(ohmd_context* ctx, unsigned int (*routine)(void* arg), void* arg);
 void ohmd_destroy_thread(ohmd_thread* thread);
