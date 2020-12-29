b9fb4bea21fbba317d4a6e2df0b96c9de4dd8a82
Jan Schmidt <jan@centricular.com>
2020-12-29 22:24:59 +1100

    Import UKF filter and deps
    Import a new implementation of an Unscented Kalman Filter and a
    basic 2D matrix helper that implements enough matrix functions for the
    filter to run.

---
+++ src/ukf.c
@@ -0,0 +1,269 @@
+// Copyright 2020, Jan Schmidt
+// SPDX-License-Identifier: BSL-1.0
+#include <stdio.h>
+
+#include "ukf.h"
+
+static void print_mat(const char *label, const matrix2d *mat)
+{
+#if 0
+    int i, j;
+
+    if (label)
+        printf ("%s: %u rows, %u cols [\n", label, mat->m, mat->n);
+
+    for (i = 0; i < mat->m; i++) {
+        printf ("  [");
+        for (j = 0; j < mat->n-1; j++) {
+            printf ("%8.4f, ", mat->mat[j][i]);
+        }
+        printf ("%10.4f ],\n", mat->mat[j][i]);
+    }
+    printf("]\n");
+#endif
+}
+
+void ukf_base_init(ukf_base *u, int N_state, int N_cov, matrix2d *Q, ukf_process_fn process_fn,
+  unscented_mean_fn mean_fn, unscented_residual_fn residual_fn, unscented_sum_fn sum_fn)
+{
+  u->N_state = N_state;
+  u->N_cov = N_cov;
+
+  ut_init_van_der_merwe(&u->ut_X, u->N_state, u->N_cov, 0.1, 2.0, 1.0);
+
+  u->ut_X.mean_fn = mean_fn;
+  u->ut_X.residual_fn = residual_fn;
+  u->ut_X.sum_fn = sum_fn;
+
+  u->x_prior= matrix2d_alloc0(u->N_state, 1);
+  u->P_prior= matrix2d_alloc_identity(u->N_cov);
+
+  u->x = matrix2d_alloc(u->N_state, 1);
+  u->P = matrix2d_alloc(u->N_cov, u->N_cov);
+
+  u->Q = Q;
+
+  u->num_sigmas = u->ut_X.N_sigma;
+  u->process_fn = process_fn;
+
+  u->sigmas = matrix2d_alloc (u->N_state, u->num_sigmas);
+
+  u->X_tmp = matrix2d_alloc(u->N_state, 1);
+  u->X_tmp_prior = matrix2d_alloc(u->N_state, 1);
+
+  u->P_tmp = matrix2d_alloc(u->N_cov, u->N_cov);
+}
+
+void ukf_base_clear(ukf_base *u)
+{
+  matrix2d_free(u->x);
+  matrix2d_free(u->P);
+  if (u->Q)
+    matrix2d_free(u->Q);
+
+  matrix2d_free(u->sigmas);
+
+  matrix2d_free(u->X_tmp);
+  matrix2d_free(u->X_tmp_prior);
+
+  matrix2d_free(u->P_tmp);
+
+  ut_clear(&u->ut_X);
+}
+
+bool ukf_base_predict(ukf_base *u, double dt)
+{
+  return ukf_base_predict_with_process(u, dt, u->process_fn);
+}
+
+bool ukf_base_predict_with_process(ukf_base *u, double dt, ukf_process_fn process_fn)
+{
+  /* Generate the sigma points around the current state prior */
+  if (!ut_compute_sigma_points(&u->ut_X, u->sigmas, u->x_prior, u->P_prior)) {
+      printf ("Prediction UKF weight computation failed\n");
+      return false;
+  }
+
+  /* Loop over each sigma point state vector and transform
+   * it through the process function */
+  for (int i = 0; i < u->num_sigmas; i++) {
+    if (matrix2d_extract_column(u->X_tmp_prior, u->sigmas, i) != MATRIX_RESULT_OK)
+      return false;
+
+    if (!process_fn(u, dt, u->X_tmp_prior, u->X_tmp))
+      return false;
+
+    if (matrix2d_replace_column(u->sigmas, i, u->X_tmp) != MATRIX_RESULT_OK)
+      return false;
+  }
+
+  if (!ut_compute_transform (&u->ut_X, u->sigmas, u->x, u->P, u->Q)) {
+      printf ("Failed to compute UKF transformed mean and covariance\n");
+      return false;
+  }
+
+  /* Fix-up numerical errors in the computed covariance */
+  if (matrix2d_condition_symmetry(u->P, 1e-26) != MATRIX_RESULT_OK)
+    return false;
+
+  /* Re-generate the sigma points, because the covariance has been changed */
+  if (!ut_compute_sigma_points(&u->ut_X, u->sigmas, u->x, u->P)) {
+      printf ("UKF weight re-draw computation failed\n");
+      return false;
+  }
+
+  return true;
+}
+
+bool
+ukf_base_commit(ukf_base *u)
+{
+  if (matrix2d_copy(u->x_prior, u->x) != MATRIX_RESULT_OK)
+    return false;
+  if (matrix2d_copy(u->P_prior, u->P) != MATRIX_RESULT_OK)
+    return false;
+
+  return true;
+}
+
+bool
+ukf_base_update(ukf_base *u, ukf_measurement *m)
+{
+  int i;
+
+  /* Loop over each sigma point state vector and transform
+   * it through the measurement function and into the measurement
+   * sigmas matrix */
+  for (i = 0; i < u->num_sigmas; i++) {
+    if (matrix2d_extract_column(u->X_tmp, u->sigmas, i) != MATRIX_RESULT_OK)
+      return false;
+
+    if (!m->measurement_fn(u, m, u->X_tmp, m->Z_est))
+      return false;
+
+    if (matrix2d_replace_column(m->sigmas, i, m->Z_est) != MATRIX_RESULT_OK)
+      return false;
+  }
+
+  if (!ut_compute_transform (&m->ut_Z, m->sigmas, m->Z_est, m->Pz, m->R)) {
+      printf ("Failed to compute UKF transformed mean and covariance of measurement\n");
+      return false;
+  }
+
+  print_mat("Measurement sigmas", m->sigmas);
+  //print_mat("Measurement weights", m->ut_Z.w_means);
+
+  print_mat("Measurement estimated Z", m->Z_est);
+  print_mat("Measurement covariance", m->Pz);
+
+  /* Compute cross-variance Pxz of the state and the measurement */
+  if (!ut_compute_crossvariance(&u->ut_X, &m->ut_Z, u->sigmas, u->x, m->sigmas, m->Z_est, m->Pxz))
+    return false;
+
+  print_mat("Cross-variance Pxz", m->Pxz);
+
+  /* Compute Kalman gain K = Pxz * Pz^-1 */
+  /* The matrix invert function destroys the input, so make a copy */
+  if (matrix2d_copy(m->Pz_tmp1, m->Pz) != MATRIX_RESULT_OK)
+    return false;
+  if (matrix2d_invert(m->Pz_tmp2, m->Pz_tmp1) != MATRIX_RESULT_OK)
+    return false;
+  if (matrix2d_multiply(m->K, m->Pxz, m->Pz_tmp2) != MATRIX_RESULT_OK)
+    return false;
+
+  /* Compute innovation y */
+  if (m->ut_Z.residual_fn) {
+    if (!m->ut_Z.residual_fn(&m->ut_Z, m->z, m->Z_est, m->y))
+      return false;
+  }
+  else {
+    if (matrix2d_subtract(m->y, m->z, m->Z_est) != MATRIX_RESULT_OK)
+      return false;
+  }
+
+  print_mat("Kalman gain", m->K);
+  print_mat("Innovation", m->y);
+
+  /* scale innovation by the Kalman gain */
+  if (matrix2d_multiply(u->ut_X.P_var, m->K, m->y) != MATRIX_RESULT_OK)
+    return false;
+
+  print_mat("State adjustment matrix", u->ut_X.P_var);
+
+  /* Update the state by the adjustment matrix to be the new
+   * x_prior */
+
+  /* Use non-linear state update function if provided */
+  if (u->ut_X.sum_fn) {
+    if (!u->ut_X.sum_fn(&u->ut_X, u->x, u->ut_X.P_var, u->x_prior))
+      return false;
+  }
+  else {
+    if (matrix2d_add(u->x_prior, u->x, u->ut_X.P_var) != MATRIX_RESULT_OK)
+      return false;
+  }
+
+  /* update P_prior = P_prior - K Py K^T */
+  if (matrix2d_multiplyXYXt(u->P_tmp, m->K, m->Pz) != MATRIX_RESULT_OK)
+    return false;
+  if (matrix2d_subtract(u->P_prior, u->P, u->P_tmp) != MATRIX_RESULT_OK)
+    return false;
+  if (matrix2d_condition_symmetry(u->P_prior, 1e-26) != MATRIX_RESULT_OK)
+    return false;
+
+  print_mat("Covariance adjustment matrix", u->P_tmp);
+
+  return true;
+}
+
+void ukf_measurement_init(ukf_measurement *m, int N_measurement, int N_cov, const ukf_base *u, ukf_measurement_fn measurement_fn,
+  unscented_mean_fn mean_fn, unscented_residual_fn residual_fn, unscented_sum_fn sum_fn)
+{
+  m->N_measurement = N_measurement;
+  m->N_cov = N_cov;
+
+  /* Initialise UT for our measurement vector dimension */
+  ut_init_matching(&m->ut_Z, N_measurement, N_cov, &u->ut_X);
+  m->ut_Z.mean_fn = mean_fn;
+  m->ut_Z.residual_fn = residual_fn;
+  m->ut_Z.sum_fn = sum_fn;
+  
+  m->num_sigmas = m->ut_Z.N_sigma;
+  m->measurement_fn = measurement_fn;
+
+  m->z = matrix2d_alloc0(N_measurement, 1);
+  m->R = matrix2d_alloc0(N_cov, N_cov);
+
+  m->y = matrix2d_alloc(N_cov, 1);
+
+  /* Kalman gain from the last update */
+  m->K = matrix2d_alloc(u->N_cov, N_cov);
+
+  m->sigmas = matrix2d_alloc (N_measurement, m->num_sigmas);
+  m->Z_est = matrix2d_alloc(N_measurement, 1);
+  m->Pz = matrix2d_alloc(N_cov, N_cov);
+
+  m->Pxz = matrix2d_alloc(u->N_cov, N_cov);
+  m->Pz_tmp1 = matrix2d_alloc(N_cov, N_cov);
+  m->Pz_tmp2 = matrix2d_alloc(N_cov, N_cov);
+}
+
+void ukf_measurement_clear(ukf_measurement *m)
+{
+  matrix2d_free(m->z);
+  matrix2d_free(m->R);
+
+  matrix2d_free(m->y);
+  matrix2d_free(m->K);
+
+  matrix2d_free(m->Pxz);
+
+  matrix2d_free(m->Pz);
+
+  matrix2d_free(m->Pz_tmp1);
+  matrix2d_free(m->Pz_tmp2);
+  matrix2d_free(m->Z_est);
+  matrix2d_free(m->sigmas);
+
+  ut_clear(&m->ut_Z);
+}
---
+++ src/unscented.c
@@ -0,0 +1,386 @@
+// Copyright 2020, Jan Schmidt
+// SPDX-License-Identifier: BSL-1.0
+#include <stddef.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "unscented.h"
+
+static void print_mat(const char *label, const matrix2d *mat)
+{
+#if 1
+    int i, j;
+
+    if (label)
+        printf ("%s: %u rows, %u cols [\n", label, mat->m, mat->n);
+
+    for (i = 0; i < mat->m; i++) {
+        printf ("  [");
+        for (j = 0; j < mat->n-1; j++) {
+            printf ("%8.4f, ", mat->mat[j][i]);
+        }
+        printf ("%10.4f ],\n", mat->mat[j][i]);
+    }
+    printf("]\n");
+#endif
+}
+
+/*
+ * From the van der Merwe paper
+ * Sigma-Point Kalman Filters for Nonlinear Estimation and Sensor-Fusion
+ * DOI: 10.2514/6.2004-5120
+ *
+ * N_state is the dimension of the state vector.
+ * N_cov is the dimension of the covariance matrices - which can be different
+ * to the state vector if sum/residual functions are used to do custom operations.
+ *
+ * alpha, beta, kappa are tuning parameters controlling the distribution
+ * of the sigma points.
+ */
+void
+ut_init_van_der_merwe(unscented_transform *ut, uint16_t N_state, uint16_t N_cov, double alpha, double beta, double kappa)
+{
+  double L_d = N_cov;
+  int i;
+
+  ut->N_state = (int) N_state;
+  ut->N_cov = (int) N_cov;
+
+  ut->lambda = alpha*alpha * (L_d + kappa) - L_d;
+
+  ut->mean_fn = NULL;
+  ut->residual_fn = NULL;
+  ut->sum_fn = NULL;
+
+  int N_sigma = ut->N_sigma = 2*ut->N_cov+1;
+
+  double w_C = 0.5 / (L_d + ut->lambda);
+
+  ut->w_means = matrix2d_alloc (N_sigma, 1);
+  ut->w_cov = matrix2d_alloc (N_sigma, 1);
+
+  /* Working space for the cholesky decomp and UKF transform */
+  ut->P_root = matrix2d_alloc (ut->N_cov, ut->N_cov);
+  ut->P_var = matrix2d_alloc (ut->N_cov, 1);
+  ut->X_tmp = matrix2d_alloc (ut->N_state, 1);
+
+  ut->w_means->mat1D[0] = ut->lambda / (L_d + ut->lambda);
+  ut->w_cov->mat1D[0] = ut->lambda / (L_d + ut->lambda) + (1.0 - alpha*alpha + beta);
+
+  for (i = 1; i < N_sigma; i++)
+    ut->w_means->mat1D[i] = ut->w_cov->mat1D[i] = w_C;
+}
+
+/*
+ * From the Julier & Uhlmann original paper
+ * A new extension of the Kalman filter to nonlinear systems
+ * DOI:10.1117/12.280797
+ *
+ * N_state is the dimension of the state vector.
+ * N_cov is the dimension of the covariance matrices - which can be different
+ * to the state vector if sum/residual functions are used to do custom operations.
+ *
+ * kappa is a weighting parameter controlling the emphasis on the original mean vs
+ * sigma points.
+ */
+void
+ut_init_julier(unscented_transform *ut, uint16_t N_state, uint16_t N_cov, double kappa)
+{
+  double L_d = N_cov;
+  int i;
+
+  ut->N_state = (int) N_state;
+  ut->N_cov = (int) N_cov;
+
+  ut->lambda = kappa;
+
+  ut->mean_fn = NULL;
+  ut->residual_fn = NULL;
+  ut->sum_fn = NULL;
+
+  int N_sigma = ut->N_sigma = 2*ut->N_cov+1;
+
+  double w_C = 0.5 / (L_d + ut->lambda);
+
+  ut->w_means = matrix2d_alloc (N_sigma, 1);
+  ut->w_cov = matrix2d_alloc (N_sigma, 1);
+
+  /* Working space for the cholesky decomp and UKF transform */
+  ut->P_root = matrix2d_alloc (ut->N_cov, ut->N_cov);
+  ut->P_var = matrix2d_alloc (ut->N_cov, 1);
+  ut->X_tmp = matrix2d_alloc (ut->N_state, 1);
+
+  ut->w_cov->mat1D[0] = ut->w_means->mat1D[0] = ut->lambda;
+  for (i = 1; i < N_sigma; i++)
+    ut->w_means->mat1D[i] = ut->w_cov->mat1D[i] = w_C;
+}
+
+/* Set up a UT from an existing one, with the same sigma points distribution but
+ * a different state/measurement vector dimension L */
+void
+ut_init_matching(unscented_transform *ut, uint16_t N_state, uint16_t N_cov, const unscented_transform *ut_src)
+{
+  ut->N_state = (int) N_state;
+  ut->N_cov = (int) N_cov;
+  ut->N_sigma = ut_src->N_sigma;
+  ut->lambda = ut_src->lambda;
+
+  ut->mean_fn = NULL;
+  ut->residual_fn = NULL;
+  ut->sum_fn = NULL;
+
+  ut->w_means = matrix2d_dup (ut_src->w_means);
+  ut->w_cov = matrix2d_dup (ut_src->w_cov);
+
+  /* Working space for the cholesky decomp and UKF transform */
+  ut->P_root = matrix2d_alloc (ut->N_cov, ut->N_cov);
+  ut->P_var = matrix2d_alloc (ut->N_cov, 1);
+  ut->X_tmp = matrix2d_alloc (ut->N_state, 1);
+}
+
+void
+ut_clear(unscented_transform *ut)
+{
+  matrix2d_free (ut->w_means);
+  matrix2d_free (ut->w_cov);
+  matrix2d_free (ut->P_root);
+  matrix2d_free (ut->P_var);
+  matrix2d_free (ut->X_tmp);
+}
+
+/* Calculate the array of sigma-points (sigmas) around the given
+ * mean and covariance using the weights matrix
+ *
+ * Input:
+ *   mean: 1xN_state means matrix
+ *    cov: N_cov x N_cov covariance matrix
+ * Output:
+ * sigmas: N_sigma x N_cov sigma points (N_sigma columns of N_cov rows)
+ */
+bool
+ut_compute_sigma_points(const unscented_transform *ut, matrix2d *sigmas, const matrix2d *mean, const matrix2d *cov)
+{
+  double L_d = ut->N_cov;
+
+  /* Means is a single column of L values */
+  if (mean->m != ut->N_state || mean->n != 1)
+    return false;
+
+  /* Covariance matrix must be square */
+  if (cov->m != ut->N_cov || cov->n != ut->N_cov)
+    return false;
+
+  /* We need space for 2*n + 1 sigma point columns, each N_state rows */
+  if (sigmas->m != ut->N_state || sigmas->n != ut->N_sigma)
+    return false;
+
+  if (matrix2d_multiply_scalar (ut->P_root, cov, ut->lambda + L_d) != MATRIX_RESULT_OK)
+    return false;
+
+  if (matrix2d_cholesky_in_place (ut->P_root) != MATRIX_RESULT_OK) {
+    print_mat("Cholesky decomposition failed on P_root", ut->P_root);
+    return false;
+  }
+
+  /* External sum_fn - extract each column of the P_root and
+   * generate the sigma point using it */
+  if (ut->sum_fn != NULL) {
+    /* zero-th entry is the unmodified state vector */
+    for (int i = 0; i < ut->N_state; i++) {
+      sigmas->mat[0][i] = mean->mat1D[i];
+    }
+
+    for (int i = 0; i < ut->N_cov; i++) {
+      /* Implicit transpose of the P_root matrix here, because the
+       * cholesky calc generates the lower-triangular matrix. */
+      /* Generate X = X_prior + P_var sigma points */
+      for (int j = 0; j < ut->N_cov; j++) {
+        ut->P_var->mat1D[j] = ut->P_root->mat[i][j];
+      }
+      if (!ut->sum_fn(ut, mean, ut->P_var, ut->X_tmp))
+        return false;
+      if (matrix2d_replace_column(sigmas, i+1, ut->X_tmp) != MATRIX_RESULT_OK)
+        return false;
+
+      /* Repeat for X = X_prior - P_var */
+      for (int j = 0; j < ut->N_cov; j++) {
+        ut->P_var->mat1D[j] = -ut->P_root->mat[i][j];
+      }
+      if (!ut->sum_fn(ut, mean, ut->P_var, ut->X_tmp))
+        return false;
+      if (matrix2d_replace_column(sigmas, ut->N_cov+i+1, ut->X_tmp) != MATRIX_RESULT_OK)
+        return false;
+    }
+  } else {
+    for (int i = 0; i < ut->N_cov; i++) {
+      sigmas->mat[0][i] = mean->mat1D[i];
+
+      for (int j = 0; j < ut->N_cov; j++) {
+        /* Implicit transpose of the P_root matrix here, because the
+         * cholesky calc generates the lower-triangular matrix. */
+        sigmas->mat[j+1][i] = mean->mat1D[i] + ut->P_root->mat[j][i];
+        sigmas->mat[ut->N_cov+j+1][i] = mean->mat1D[i] - ut->P_root->mat[j][i];
+      }
+    }
+  }
+
+  return true;
+}
+
+/* Compute the new mean and covariance of transformed
+ * sigma points, weighted by the weights array stored
+ * in the unscented_transform struct.
+ *
+ * This is the 2nd equations in step 3 (Time-update
+ * equations) of the UKF section of the paper,
+ *
+ * Used for the predict step to compute the predicted
+ * means and covariance, or in the update step
+ * to compute the measurement update means and covariances.
+ *
+ * If a noise matrix is provided, it is added to the final
+ * covariance matrix.
+ *
+ * Input:
+ * sigmas: N_sigma x N_cov sigma points (N_sigma columns of N_cov rows)
+ * noise: N_cov x N_cov noise matrix to add to the covariance (optional)
+ *
+ * Output:
+ *
+ * mean: 1xN_state state vector
+ * cov: N_cov x N_cov covariance matrix
+ *
+ */
+bool
+ut_compute_transform (const unscented_transform *ut, const matrix2d *sigmas, matrix2d *mean, matrix2d *cov, matrix2d *noise)
+{
+  if (sigmas->m != mean->m || sigmas->n != ut->N_sigma)
+    return false;
+
+  if (cov->m != ut->N_cov || cov->n != ut->N_cov)
+    return false;
+
+  if (noise) {
+    if (cov->m != noise->m || cov->n != noise->n)
+      return false;
+  }
+
+  /* Mean is the sum of the sigmas (LxN) * weights (1xN) */
+  if (ut->mean_fn == NULL) {
+    if (matrix2d_multiply(mean, sigmas, ut->w_means) != MATRIX_RESULT_OK) {
+      return false;
+    }
+  } else {
+    /* Call the callback for computing non-linear means */
+    if (!ut->mean_fn(ut, sigmas, ut->w_means, mean))
+      return false;
+  }
+
+  /* Compute NxN covariance matrix */
+  int i, j, k;
+
+  /* Clear the output covariance matrix.
+   * FIXME: This could be skipped with an if check
+   * in the loop below to set instead of accumulate
+   * on the first loop. Not sure if that'll be any
+   * quicker for small matrices */
+  for (i = 0; i < ut->N_cov * ut->N_cov; i++) {
+    cov->mat1D[i] = 0.0;
+  }
+
+  for (k = 0; k < ut->N_sigma; k++) {
+    /* Compute residual for this sigma point */
+    if (ut->residual_fn == NULL) {
+      for (j = 0; j < ut->N_cov; j++) {
+        ut->P_var->mat1D[j] = sigmas->mat[k][j] - mean->mat1D[j];
+      }
+    } else {
+      /* Use external residual func ptr, to compute residuals with
+       * methods other than direct subtraction */
+      if (matrix2d_extract_column(ut->X_tmp, sigmas, k) != MATRIX_RESULT_OK)
+        return false;
+      if (!ut->residual_fn(ut, ut->X_tmp, mean, ut->P_var))
+        return false;
+    }
+
+    /* Add the weighted version to the covariance matrix using
+     * outer product */
+    double w_C = ut->w_cov->mat1D[k];
+    double *P_var = ut->P_var->mat1D;
+
+    /* Optimised by looping over the cov array directly instead
+     * of via pointers */
+    double *cov_ptr = cov->mat1D;
+    for (i = 0; i < ut->N_cov; i++) {
+      for (j = 0; j < ut->N_cov; j++) {
+        cov_ptr[0] += w_C * P_var[j] * P_var[i];
+        cov_ptr++;
+      }
+    }
+  }
+
+  if (noise) {
+    if (matrix2d_add_in_place (cov, noise) != MATRIX_RESULT_OK) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+static bool compute_one_residual(unscented_transform *ut, const matrix2d *sigmas, const int i, const matrix2d *mean, matrix2d *dest) {
+  int j;
+
+  assert(i >= 0 && i < ut->N_sigma);
+
+  if (ut->residual_fn == NULL) {
+    for (j = 0; j < ut->N_cov; j++) {
+      dest->mat1D[j] = sigmas->mat[i][j] - mean->mat1D[j];
+    }
+  } else {
+    /* Use external residual func ptr, to compute residuals with
+     * methods other than direct subtraction */
+    if (matrix2d_extract_column(ut->X_tmp, sigmas, i) != MATRIX_RESULT_OK)
+      return false;
+    if (!ut->residual_fn(ut, ut->X_tmp, mean, dest))
+      return false;
+  }
+  return true;
+}
+
+bool ut_compute_crossvariance(unscented_transform *utX, unscented_transform *utZ, const matrix2d *sigmasX, const matrix2d *meanX, const matrix2d *sigmasZ, const matrix2d *meanZ, matrix2d *Pxz)
+{
+  int i, j, k;
+  double *Pxz_ptr = Pxz->mat1D;
+  double *P_var_X, *P_var_Z;
+
+  /* Clear the Pxz matrix */
+  for (i = 0; i < utX->N_cov * utZ->N_cov; i++)
+    *Pxz_ptr++ = 0.0;
+
+  for (k = 0; k < utX->N_sigma; k++) {
+    /* Compute residuals for this sigma point */
+    if (!compute_one_residual(utX, sigmasX, k, meanX, utX->P_var))
+      return false;
+    if (!compute_one_residual(utZ, sigmasZ, k, meanZ, utZ->P_var))
+      return false;
+
+    /* Add the weighted version to the cross-variance matrix using
+     * outer product */
+    double w_C = utX->w_cov->mat1D[k];
+
+    /* Optimise by looping over the Pxz and input arrays directly instead
+     * of via pointers */
+    Pxz_ptr = Pxz->mat1D;
+    P_var_X = utX->P_var->mat1D;
+    P_var_Z = utZ->P_var->mat1D;
+
+    for (j = 0; j < utZ->N_cov; j++) {
+      for (i = 0; i < utX->N_cov; i++) {
+        *Pxz_ptr++ += w_C * P_var_X[i] * P_var_Z[j];
+      }
+    }
+  }
+
+  return true;
+}
