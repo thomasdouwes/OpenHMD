8c90197ad4eaa3d89234cb5044cf30309083443d
Jan Schmidt <jan@centricular.com>
2021-06-06 23:59:57 +1000

    Add Velocity and Acceleration API
    Add enum values for querying acceleration and velocity from a device.

--- include/openhmd.h
+++ include/openhmd.h
@@ -94,6 +94,12 @@ typedef enum {
 	OHMD_ANALOG = 1
 } ohmd_control_type;
 
+
+/**
+ * Define that says VELOCITY and ACCELERATION properties are available in this API
+ */
+#define OHMD_HAVE_VEL_ACCEL_API_v0
+
 /** A collection of float value information types, used for getting and setting information with
     ohmd_device_getf() and ohmd_device_setf(). */
 typedef enum {
@@ -163,6 +169,11 @@ typedef enum {
 	/** float[OHMD_CONTROL_COUNT] (get): Get the state of the device's controls. */
 	OHMD_CONTROLS_STATE                = 22,
 
+	/** float[3] (get): A 3-D vector representing the velocity of the device (m/s). */
+	OHMD_VELOCITY_VECTOR                  =  23,
+
+	/** float[3] (get): A 3-D vector representing the acceleration of the device (m/s^2). */
+	OHMD_ACCELERATION_VECTOR              =  24,
 } ohmd_float_value;
 
 /** A collection of int value information types used for getting information with ohmd_device_geti(). */
